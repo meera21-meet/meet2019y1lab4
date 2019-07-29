ee79e65669cb21a903c57721fe03c8b2baa41d1e
meera21-meet <meera21@meet.mit.edu>
2019-07-29 12:57:21 -0400

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,43 @@
+import turtle
+'''turtle.shape('square')
+finn=turtle.clone()
+finn.shape('square')
+finn.forward (100)
+finn.right(90)
+finn.forward(100)
+finn.right(90)
+finn.forward(100)
+finn.right(90)
+finn.forward(100)
+
+sam.shape('triangle')
+sam.left(45)
+sam.stamp()
+sam.forward(100)
+sam.stamp()
+sam.clearstamps()
+sam.right(130)
+sam.forward(100)'''
+sam=turtle.clone()
+sam.shape('circle')
+turtle.bgcolor('purple')
+sam.color('white')           
+sam.stamp()
+
+
+
+sam.goto(0,0)
+sam.pensize(12)
+sam.goto(0,100)
+sam.color('yellow')
+sam.goto(50,100)
+sam.goto(50,0)
+sam.color('green')
+sam.goto(50,-100)
+sam.goto(100,-100)
+sam.color('blue')
+sam.shape('arrow')
+sam.goto(100,0)
+def up():
+    
+
